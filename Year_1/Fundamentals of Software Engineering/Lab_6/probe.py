39efac3019663d64c976b58609dac49c5c4d817c
Ivan <zhdanovich2818@gmail.com>
2025-12-07 12:39:15 +0300

    Revert "sync"
    This reverts commit eddf957646c1baf537e83be1da9c1219ccc5b428.

--- Year_1/Fundamentals of Software Engineering/Lab_6/probe.py
+++ Year_1/Fundamentals of Software Engineering/Lab_6/probe.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import xml.etree.ElementTree as ET
-from operator import index
+
+from numpy.ma.core import append
 
 
 def load_users_data() -> list[dict]:
@@ -24,7 +27,7 @@ def load_users_data() -> list[dict]:
 def load_workouts_data() -> list[dict]:
     try:
         workouts_tree = ET.parse('workouts.xml')
-        workouts_stats = []
+        workouts = []
         for workouts_elem in workouts_tree.getroot().findall('workout'):
             workout = {
                 'workout_id': int(workouts_elem.find('workout_id').text),
@@ -37,8 +40,8 @@ def load_workouts_data() -> list[dict]:
                 'avg_heart_rate': workouts_elem.find('avg_heart_rate').text,
                 'intensity': workouts_elem.find('intensity').text,
             }
-            workouts_stats.append(workout)
-        return workouts_stats
+            workouts.append(workout)
+        return workouts
     except FileNotFoundError:
         print("Файл не найден")
         return []
@@ -108,10 +111,10 @@ def analyze_user_activity(users_list: list[dict], workouts_stats: list[dict]):
     for person in ranked_people[:3]:
         print(
 f'''{person["name"]} ({person['level']}) 
-Рейтинг: {person['score']:.1f}
-Тренировок: {person['workouts']}
-Калорий: {person['calories']}
-Время: {(person['total_time']/60):.1f} часов
+ Рейтинг: {person['score']:.1f}
+ Тренировок: {person['workouts']}
+ Калорий: {person['calories']}
+ Время: {(person['total_time']/60):.1f} часов
 ''')
 
 
@@ -126,33 +129,66 @@ def analyze_workout_types(workouts_stats: list[dict]):
             types[workout['type']][1] += workout['duration']
             types[workout['type']][2] += workout['calories']
     print("РАСПРЕДЕЛЕНИЕ ПО ТИПАМ ТРЕНИРОВОК:")
+    y = []
     for type in types:
         types[type][1] /= types[type][0]
         types[type][2] /= types[type][0]
+        y += (types[type][0]/workouts_stats[-1]['workout_id'])*100
         print(
-f'''{type}: {types[type][0]} тренировок ({(types[type][0]/workouts_stats[-1]['workout_id'])*100:.1f}%)
+f'''{type.capitalize()}: {types[type][0]} тренировок ({(types[type][0]/workouts_stats[-1]['workout_id'])*100:.1f}%)
  Средняя длительность: {int(types[type][1])} мин
  Средние калории: {int(types[type][2])} ккал
 ''')
+    plt.pie(y, labels=types, autopct='%1.1f%%')
+    plt.show()
 
 
 
 
+def find_user_by_name(users_list: list[dict], name: str):
+    user = next((u for u in users_list if u['name'].lower() == name.lower()), None)
+    return user
+
+def find_user_workouts(users_list: list[dict], user_name: str) -> list[dict]:
+    user = find_user_by_name(users_list, user_name)
+    user_workouts = user['workouts']
+    return user_workouts
 
 
 
+def analyze_user(users_list: list[dict], user_name: str):
+    user = find_user_by_name(users_list, user_name)
+    workouts = find_user_workouts(users_list, user_name)
+    calories_burned = 0
+    total_time = 0
+    count_workouts = len(workouts)
+    distance = 0
+    avg_calories = 0
+    type = {}
+    for w in workouts:
+        calories_burned += w['calories']
+        total_time += w['duration']
+        distance += w['distance']
+        if w['type'] not in type:
+            type[w['type']] = 1
+        else:
+            type[w['type']] += 1
 
-#         print(f'''
-# user: {user["name"]} ({user['fitness_level']})
-# workouts: {count_workouts}
-# calories: {calories_burned}
-# time: {total_time/60:.1f}
-#     ''')
-
-
-
-        # work = "\n".join(f"{w['workout_id']} - {w['user_id']} - {w['date']} - {w['type']}" for w in user['workouts'])
-        # print(user['name'] + " \n" + work)
+    avg_calories = calories_burned / count_workouts
+    type = max(type, key=lambda x: x[1])
+    print(
+f'''
+ДЕТАЛЬНЫЙ АНАЛИЗ ДЛЯ ПОЛЬЗОВАТЕЛЯ: {user['name']}
+===========================================
+Возраст: {user['age']} лет, Вес: {user['weight']} кг
+Уровень: {user['fitness_level']}
+Тренировок: {count_workouts}
+Сожжено калорий: {calories_burned}
+Общее время: {total_time/60:.1f} часов
+Пройдено дистанции: {distance:.1f} км
+Средние калории за тренировку: {avg_calories:.0f}
+Любимый тип тренировки: {type}
+''')
 
 
 
@@ -160,16 +196,20 @@ f'''{type}: {types[type][0]} тренировок ({(types[type][0]/workouts_sta
 users = load_users_data()
 workouts = load_workouts_data()
 
+get_stats(users, workouts)
 analyze_workout_types(workouts)
 analyze_user_activity(users, workouts)
 
-# get_stats(users, workouts)
+
+for w in find_user_workouts(users, "аННа"):
+    print(f"{w['workout_id']} {w['type']} {w['duration']} {w['calories']}")
+
+
+analyze_user(users, "Борис")
 
 
 
 
-
-
-
+# user = next((u for u in users if u['name'].lower() == user_name.lower()), None)
 
 
